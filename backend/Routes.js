fe16b0f7e9152577ca9bb942cb77e72428199928
Udit bhadauria <UDITBHADAURIA.23041058@gehu.ac.in>
2024-02-29 19:11:08 +0530

    done

--- backend/Routes.js
+++ backend/Routes.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors({}))
 
 app.get('/', (req, res) => { 
-    res.send("Hello Worldd");
+    res.send("Hello Worlddd");
 })
 
 app.post('/todos',async (req, res) => {
