fee84785662a2d7605cba8d4f221c0c09a3a8389
Udit bhadauria <UDITBHADAURIA.23041058@gehu.ac.in>
2024-02-29 22:44:03 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import { Todos } from './components/Todos'
 function App() {
   const [todos, setTodos] = useState([]);
 
-  // fetch("http://localhost:3000/todo")
+  // fetch("http://localhost:3000/todos")
   //   .then(async function(res) {
   //     const json = await res.json();
   //     setTodos(json.todos);
--- frontend/src/components/createtodo.jsx
+++ frontend/src/components/createtodo.jsx
@@ -1,29 +1,47 @@
-import React, { useState } from 'react'; // Import React and useState hook
+import { useState } from "react";
 
-export function CreateTodo() { // Use PascalCase for component names
-  const [title, setTitle] = useState(""); // State for todo title
-  const [description, setDescription] = useState(""); // State for todo description
+export function CreateTodo(props) {
+    // react-query
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
 
-  return (
-    <div>
-      <input 
-        type="text" 
-        id="title" 
-        name="title" 
-        placeholder="Title" 
-        value={title} // Bind input value to title state
-        onChange={(e) => setTitle(e.target.value)} // Update title state on input change
-      /><br />
-      <input 
-        type="text" 
-        id="description" 
-        name="description" 
-        placeholder="Description" 
-        value={description} // Bind input value to description state
-        onChange={(e) => setDescription(e.target.value)} // Update description state on input change
-      /><br />
-      <button>add todo</button>
+    return <div>
+        <input id="title" style={{
+            padding: 10,
+            margin: 10
+        }} type="text" placeholder="title" onChange={function(e) {
+            const value = e.target.value;
+            setTitle(e.target.value);
+        }}></input> <br />
+    
+        <input id="desc" style={{
+            padding: 10,
+            margin: 10
+        }} type="text" placeholder="description" onChange={function(e) {
+            const value = e.target.value;
+            setDescription(e.target.value);
+        }}></input> <br />
+
+        <button style={{
+            padding: 10,
+            margin: 10
+        }} onClick={() => {
+            // axios
+            fetch("http://localhost:3000/todo", {
+                method: "POST",
+                body: JSON.stringify({
+                    title: title,
+                    description: description
+                }),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            })
+                .then(async function(res) {
+                    const json = await res.json();
+                    alert("Todo added");
+                })
+        }}>Add a todo</button>
     </div>
-  );
 }
 
